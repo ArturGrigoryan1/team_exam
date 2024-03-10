5afe46209cdac29906e7ee38e5345d0760353e22
ArturGrigoryan1 <143454080+ArturGrigoryan1@users.noreply.github.com>
2024-03-10 19:23:42 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print('welcome to calendar\n')
+
+year = int(input('please lead the year: '))
+month = int(input('please lead the any month number: '))
+
+print(calendar.month(year, month))
+
+print('good luck')
